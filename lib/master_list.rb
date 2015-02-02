0951b6879826dfa93dd8491a2a326e9d2dc0a445
Megan Glazner <megan.glazner@gmail.com>
2015-02-01 21:30:46 -0800

    Pretty rough, returning the result pretty bluntly, but it served the purpose, at least for now. I expect it to be changed before long.

--- lib/master_list.rb
+++ lib/master_list.rb
@@ -15,4 +15,30 @@ class MasterList
 		end
 	end
 	
+	def complete
+		@list_items.select do |i|
+			i.status_of_task==true
+		end
+	end
+	
+	def filter_by_date(day)
+		temp=""
+		@list_items.each do |i|
+			if i.due_date==day
+					temp<<i.task
+			end
+		end
+		temp	
+	end
+	
+	def search_by_term(term)
+		temp=""
+		@list_items.each do |i|
+			if i.task.include?(term)
+				temp<<i.task
+			end	
+		end
+		temp
+	end
+	
 end
--- test/master_list_test.rb
+++ test/master_list_test.rb
@@ -21,8 +21,29 @@ class MasterListTest < MiniTest::Unit::TestCase
 		assert_equal([item1],tester.incomplete)
 	end
 
+	def test_complete
+		item1=Item.new("walk the dog","Tuesday",false)
+		item2=Item.new("feed the cat","Wednesday",true)
+		tester=MasterList.new([item1,item2])
+		assert_equal([item2],tester.complete)
+	end
+	
+	def test_filter_by_date
+		item1=Item.new("feed the dog","Tuesday",false)
+		item2=Item.new("feed the cat","Wednesday",true)
+		tester=MasterList.new([item1,item2])
+		assert_equal(item1.task,tester.filter_by_date("Tuesday"))
+		assert_equal(item2.task,tester.filter_by_date("Wednesday"))
+	end
+	
+	def test_search_by_term
+		item1=Item.new("walk the dog","Tuesday",false)
+		item2=Item.new("feed the cat","Wednesday",true)
+		tester=MasterList.new([item1,item2])
+		assert_equal(item1.task,tester.search_by_term("walk"))
+		assert_equal(item2.task,tester.search_by_term("cat"))
+	end
+	
 end
-
-
 
 ## DUE FEB 3, extend MasterList, add at least three methods and their tests, suggest: .complete (negation of current method) due by day? (direct comparison), and find by text, (anywhere in string contains keyword?)
